77d84cb12cac0391346a878feb6c580c3e6e83b6
guoma532 <guoma532@gmail.com>
2024-01-30 00:01:46 -0500

    180 on starter code to put on time function

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,5 +1,19 @@
-const questions = [
-    {
-        question: ""
-    }
+let tieLeft = document.querySelector(".time-left");
+let quizContainer   =   document.getElementById("container");
+let nextBtn =   document.getElementById("next-button");
+let countOfQuestion =   document.querySelector(".numer-of-questions");
+let displayCounter  =   document.getElementById("display-container");
+let restart = document.getElementById("restart");
+let userScore = document.getElementById("user-score");
+let startScreen = document.querySelector("start-screen");
+let startButton = document.getElementById("start-button");
+let questionCount;
+let scoreCount = 0:
+let count= 11;
+let countdown;
+
+const quizArray = [
+
 ]
+
+// -work in rogress-
